ad3f8c76d10cb474ae0ff9a3819d4e65800e248d
Manu Munoz <manus.reload@gmail.com>
2021-01-16 22:45:09 +0100

    fix for merge unrelated histories

---
+++ config/config.inc.php
@@ -0,0 +1,19 @@
+<?php
+global $DATABASE;
+global $config;
+
+$config['database.host'] = "localhost";
+$config['database.user'] = "admin_crash";
+$config['database.pass'] = "";
+$config['database.name'] = "admin_crash";
+
+$config['smtp.enabled'] = false;
+$config['smtp.hostname'] = "localhost";
+$config['smtp.username'] = "";
+$config['smtp.password'] = "";
+$config['smtp.ssl'] = false;
+
+
+$config['google.api'] = ""; // Google server API key
+
+
